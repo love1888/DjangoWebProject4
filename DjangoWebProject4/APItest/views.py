33978b01a20c7cf6b47566ab7946f3f694f92cf4
love1888 <41221796+love1888@users.noreply.github.com>
2019-07-15 19:57:04 +0800

    edit test

--- DjangoWebProject4/APItest/views.py
+++ DjangoWebProject4/APItest/views.py
@@ -4,10 +4,4 @@ from django.http import HttpResponse
 from datetime import datetime
 
 def index(request):
-    now = datetime.now()
-
-    html_content = "<html><head><title>Hello, Django</title></head><body>"
-    html_content += "<strong>Hello Djanghgi!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
-    html_content += "</body></html>"
-
-    return HttpResponse(html_content)
+	return HttpResponse("Hello, Django!")
